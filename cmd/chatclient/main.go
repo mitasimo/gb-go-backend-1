65a74e43ebdc92ac4c0d46596aef281725421c26
Dmitry Simonenko <d.simonenko@faberlic.com>
2021-12-27 16:50:59 +0300

    added named client connections

--- cmd/chatclient/main.go
+++ cmd/chatclient/main.go
@@ -9,11 +9,25 @@ import (
 )
 
 func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("usage: chatclient username")
+		return
+	}
+
 	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
+	n, err := io.WriteString(conn, os.Args[1]+"\n")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("send %d bytes\n", n)
+
 	go func() {
 		io.Copy(os.Stdout, conn)
 	}()

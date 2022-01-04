db6c4f84af3eb9e6fc07da520a01c49d74f80c5d
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2022-01-04 20:32:51 +0300

    added filtering files by ext

--- internal/filelistener/handler.go
+++ internal/filelistener/handler.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 type Handler struct {
@@ -15,19 +17,32 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
 
+	ext := r.FormValue("ext")
+	if ext != "" && !strings.HasPrefix(ext, ".") {
+		ext = "." + ext
+	}
+
 	entries, err := os.ReadDir(h.UploadDir)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	cnt := 0
 	for _, entry := range entries {
 		fn := entry.Name()
+		if ext != "" && filepath.Ext(fn) != ext {
+			continue // расширение задано и не соответвтвует расшширению файла
+		}
 		info, err := entry.Info()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			continue
 		}
 		fmt.Fprintf(w, "%s\tsize: %d bytes\n", fn, info.Size())
+		cnt++
+	}
+	if cnt == 0 {
+		fmt.Fprint(w, "No files")
 	}
 }
